8f9fa1ae3cc60ed0ab80b89f23909355cd6fe9e9
gscontras <scontras@stanford.edu>
2015-04-28 14:34:43 -0700

    plot by predicate

--- experiments/1-faultless-disagreement/results/analysis.R
+++ experiments/1-faultless-disagreement/results/analysis.R
@@ -15,6 +15,7 @@ d$class <- factor(d$class,levels=c("quality","size","age","texture","shape","col
 
 table(d$class,d$nounclass)
 
+## class plot by noun type
 d_s = bootsSummary(data=d, measurevar="response", groupvars=c("class","nounclass"))
 class_plot <- ggplot(d_s, aes(x=class,y=response,fill=nounclass)) +
   geom_bar(stat="identity",position=position_dodge()) +
@@ -22,3 +23,18 @@ class_plot <- ggplot(d_s, aes(x=class,y=response,fill=nounclass)) +
   ylab("faultless disagreement\n")+
   xlab("\nverb class") 
 class_plot
+ggsave("../results/class_plot.pdf")
+
+
+## predicate plot by class
+p_s = bootsSummary(data=d, measurevar="response", groupvars=c("class","predicate"))
+p_s$predicate <- factor(p_s$predicate,ordered=is.ordered(p_s$predicate))
+pred_plot <- ggplot(p_s, aes(x=reorder(predicate,-response,mean),y=response)) +
+  geom_bar(stat="identity",position=position_dodge()) +
+  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=reorder(predicate,response,is.ordered=T), width=0.1),position=position_dodge(width=0.9))+
+  ylab("faultless disagreement\n")+
+  xlab("predicate") +
+  facet_wrap(~class,scale="free_x") +
+  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))
+pred_plot
+ggsave("../results/pred_plot.pdf",height=4)
