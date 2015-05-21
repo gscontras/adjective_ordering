4ff3717eaa4366df85df3322dc5ae19220389753
gscontras <scontras@stanford.edu>
2015-05-20 17:55:56 -0700

    new plots

--- experiments/4-faultless-disagreement-2adj/results/analysis.R
+++ experiments/4-faultless-disagreement-2adj/results/analysis.R
@@ -94,8 +94,11 @@ material_agg$class = "material"
 all_agg = rbind(quality_agg,size_agg,age_agg,texture_agg,color_agg,shape_agg,material_agg)
 
 all_agg$class <- factor(all_agg$class,levels=c("quality","size","texture","age","color","shape","material"))
+all_agg$Ratio = o_agg[as.character(all_agg$configuration),]$Ratio
+all_agg[is.na(all_agg$Ratio),]$Ratio = -555
+all_agg$Preferred = as.factor(ifelse(all_agg$Ratio > 1, "preferred", ifelse(all_agg$Ratio == -555, "single","dispreferred")))
 
-all_plot <- ggplot(all_agg, aes(x=reorder(configuration,-response,mean),y=response)) +
+all_plot <- ggplot(all_agg, aes(x=reorder(configuration,-response,mean),y=response,fill=Preferred)) +
   geom_bar(stat="identity",position=position_dodge()) +
   geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=reorder(configuration,-response,mean), width=0.1),position=position_dodge(width=0.9)) +
   ylab("faultless disagreement\n") +
@@ -104,3 +107,33 @@ all_plot <- ggplot(all_agg, aes(x=reorder(configuration,-response,mean),y=respon
   theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))
 all_plot
 ggsave("../results/all_plot.pdf",width=10,height=10)
+
+agr = aggregate(response~Preferred,data=all_agg[all_agg$Preferred != "single",],FUN=mean)
+agr$YMin = agr$response - aggregate(response~Preferred,data=all_agg[all_agg$Preferred != "single",],FUN=ci.low)$response
+agr$YMax = agr$response + aggregate(response~Preferred,data=all_agg[all_agg$Preferred != "single",],FUN=ci.high)$response
+
+ggplot(agr, aes(x=Preferred,y=response)) +
+  geom_bar(stat="identity") +
+  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25)
+theme_set(theme_bw())
+
+
+## order preference data
+
+o = read.table("~/Documents/git/cocolab/adjective_ordering/experiments/3-order-preference/Submiterator-master/order-preference-trials.tsv",sep="\t",header=T)
+
+o$configuration <- paste(o$class1,o$class2)
+head(o)
+o_agg <- aggregate(response~configuration*class1*class2,data=o,mean)
+head(o_agg,20)
+
+o_agg$Ratio = -555
+row.names(o_agg) = o_agg$configuration
+
+for (first in levels(o_agg$class1)) {
+  for (second in levels(o_agg$class1)[levels(o_agg$class1) != first]) {
+    o_agg[paste(first,second),]$Ratio = o_agg[paste(first,second),]$response / o_agg[paste(second, first),]$response
+    print(paste(first,second))
+  }
+}
+
