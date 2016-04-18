799c2ff930e7959d82388840283ca022b6567e0c
gscontras <scontras@stanford.edu>
2016-04-18 10:31:10 -0700

    inherentness experiment

---
+++ experiments/14-inherentness/js/experiment-code.js
@@ -0,0 +1,180 @@
+
+
+
+
+function make_slides(f) {
+  var   slides = {};
+
+  slides.i0 = slide({
+     name : "i0",
+     start: function() {
+      exp.startT = Date.now();
+     }
+  });
+
+  slides.instructions1 = slide({
+    name : "instructions1",
+    start: function() {
+      if (exp.condition=="inherent"){
+        $("#purpose").html("essentiality and nonessentiality");
+        $("#essential").html("essential the adjective is to the meaning of the noun which it modifies, that is, how substantive or inherent"); 
+        $("#substantive").html("substantive");
+      } else {
+        $("#purpose").html("subjectivity and objectivity");
+        $("#essential").html("subjective"); 
+        $("#substantive").html("subjective");
+      }
+    }, 
+    button : function() {
+      exp.go(); //use exp.go() if and only if there is no "present" data.
+    }
+  });
+
+  slides.multi_slider = slide({
+    name : "multi_slider",
+    present : _.shuffle(stimuli),
+    present_handle : function(stim) {
+      $(".err").hide();
+      this.init_sliders();      
+      exp.sliderPost = null;
+      // $('input[name="sense"]:checked').attr('checked',false);
+      this.stim = stim; //FRED: allows you to access stim in helpers
+
+      $(".description").html(stim.Predicate + " " + stim.Noun);
+
+      $(".adjective").html(stim.Predicate);
+      $(".noun").html(stim.Noun);
+
+      if (exp.condition=="inherent"){
+        $(".objective").html("nonessential"); 
+        $(".substantive").html("essential");
+        $(".meaning").html("to the meaning of");
+      } else {
+        $(".objective").html("objective"); 
+        $(".substantive").html("subjective");
+        $(".meaning").html("when modifying");
+      };
+
+
+//      this.n_sliders = this.sentence_types.length;
+		this.n_sliders = 1;
+
+    },
+
+    button : function() {
+    	console.log(exp.sliderPost);
+      if (exp.sliderPost != null) {
+        this.log_responses();
+        _stream.apply(this); //use exp.go() if and only if there is no "present" data.
+      } else {
+        $(".err").show();
+      }
+    },
+
+    init_sliders : function() {
+      utils.make_slider("#slider0", function(event, ui) {
+        exp.sliderPost = ui.value;
+      });
+    },
+//    make_slider_callback : function(i) {
+//      return function(event, ui) {
+//        exp.sliderPost[i] = ui.value;
+//      };
+//    },
+    log_responses : function() {
+        exp.data_trials.push({
+          "response" : exp.sliderPost,
+          "noun" : this.stim.Noun,          
+          "predicate" : this.stim.Predicate,
+          "nounclass" : this.stim.NounClass,
+          "class" : this.stim.Class,  
+          "condition" : exp.condition,                    
+          // "firstutterance" : this.verbs[0],
+          // "sense" : $('input[name="sense"]:checked').val(),        
+          "slide_number" : exp.phase
+        });
+    },
+  });
+
+  slides.subj_info =  slide({
+    name : "subj_info",
+    submit : function(e){
+      //if (e.preventDefault) e.preventDefault(); // I don't know what this means.
+      exp.subj_data = {
+        language : $("#language").val(),
+        enjoyment : $("#enjoyment").val(),
+        asses : $('input[name="assess"]:checked').val(),
+        age : $("#age").val(),
+        gender : $("#gender").val(),
+        education : $("#education").val(),
+        comments : $("#comments").val(),
+      };
+      exp.go(); //use exp.go() if and only if there is no "present" data.
+    }
+  });
+
+  slides.thanks = slide({
+    name : "thanks",
+    start : function() {
+      exp.data= {
+          "trials" : exp.data_trials,
+          "catch_trials" : exp.catch_trials,
+          "system" : exp.system,
+          //"condition" : exp.condition,
+          "subject_information" : exp.subj_data,
+          "time_in_minutes" : (Date.now() - exp.startT)/60000
+      };
+      setTimeout(function() {turk.submit(exp.data);}, 1000);
+    }
+  });
+
+  return slides;
+}
+
+/// init ///
+function init() {
+  repeatWorker = false;
+  (function(){
+      var ut_id = "subjectivity";
+      if (UTWorkerLimitReached(ut_id)) {
+        $('.slide').empty();
+        repeatWorker = true;
+        alert("You have already completed the maximum number of HITs allowed by this requester. Please click 'Return HIT' to avoid any impact on your approval rating.");
+      }
+  })();
+
+  exp.trials = [];
+  exp.catch_trials = [];
+  exp.condition = _.sample(["inherent","subjective"]);
+  exp.system = {
+      Browser : BrowserDetect.browser,
+      OS : BrowserDetect.OS,
+      screenH: screen.height,
+      screenUH: exp.height,
+      screenW: screen.width,
+      screenUW: exp.width
+    };
+  //blocks of the experiment:
+  exp.structure=["i0", "instructions1",'multi_slider', 'subj_info', 'thanks'];
+  
+  exp.data_trials = [];
+  //make corresponding slides:
+  exp.slides = make_slides(exp);
+
+  exp.nQs = utils.get_exp_length(); //this does not work if there are stacks of stims (but does work for an experiment with this structure)
+                    //relies on structure and slides being defined
+
+  $('.slide').hide(); //hide everything
+
+  //make sure turkers have accepted HIT (or you're not in mturk)
+  $("#start_button").click(function() {
+    if (turk.previewMode) {
+      $("#mustaccept").show();
+    } else {
+      $("#start_button").click(function() {$("#mustaccept").show();});
+      exp.go();
+    }
+  });
+
+  exp.go(); //show first slide
+}
---
+++ experiments/14-inherentness/results/analysis.R
@@ -0,0 +1,73 @@
+library(ggplot2)
+library(lme4)
+library(hydroGOF)
+library(tidyr)
+library(dplyr)
+
+setwd("~/Documents/git/cocolab/adjective_ordering/experiments/14-inherentness/Submiterator-master")
+
+num_round_dirs = 8
+df = do.call(rbind, lapply(1:num_round_dirs, function(i) {
+  return (read.csv(paste(
+    'round', i, '/inherentness.csv', sep='')) %>%
+    #'round', i, '/inherentness-trials.csv', sep='')) %>%
+      mutate(workerid = (workerid + (i-1)*9)))}))
+
+#d = subset(df, select=c("workerid", "class","predicate","slide_number","response","condition"))
+d = subset(df, select=c("workerid", "class","predicate","slide_number","response","language","condition"))
+unique(d$language)
+#d = d[d$language!="Chinese"&d$language!="Spanish, English"&d$language!="Spanish"&d$language!="Hindi"&d$language!="spanish"&d$language!="",]
+#unique(d$language)
+
+length(unique(d$workerid)) # n=69
+
+#write.csv(d,"../results/subjectivity-expanded_results.csv")
+
+
+## predicate plot by class
+c_s = bootsSummary(data=d, measurevar="response", groupvars=c("class","condition"))
+#c_s = aggregate(response~class,data=d,mean)
+
+class_plot <- ggplot(c_s, aes(x=reorder(class,-response,mean),y=response,fill=condition)) +
+  geom_bar(stat="identity",position=position_dodge()) +
+  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=reorder(class,response,is.ordered=T), width=0.1),position=position_dodge(width=0.9))+
+  ylab("rating\n")+
+  xlab("adjective class") +
+  #facet_wrap(~class,scale="free_x") +
+  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))+
+  theme_bw()+
+  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))
+class_plot
+#ggsave("../results/class_ratings.pdf",height=3)
+
+
+
+#### just subjectivity
+
+
+## predicate plot by class
+c_s = bootsSummary(data=d, measurevar="response", groupvars=c("class"))
+class_plot <- ggplot(c_s, aes(x=reorder(class,-response,mean),y=response)) +
+  geom_bar(stat="identity",position=position_dodge()) +
+  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=reorder(class,response,is.ordered=T), width=0.1),position=position_dodge(width=0.9))+
+  ylab("subjectivity\n")+
+  xlab("adjective class") +
+  #facet_wrap(~class,scale="free_x") +
+  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))
+class_plot
+ggsave("../results/class_plot.pdf",height=5)
+
+## predicate plot by class
+p_s = bootsSummary(data=d, measurevar="response", groupvars=c("class","predicate"))
+p_s$predicate <- factor(p_s$predicate,ordered=is.ordered(p_s$predicate))
+pred_plot <- ggplot(p_s, aes(x=reorder(predicate,-response,mean),y=response)) +
+  geom_bar(stat="identity",position=position_dodge()) +
+  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=reorder(predicate,response,is.ordered=T), width=0.1),position=position_dodge(width=0.9))+
+  ylab("subjectivity\n")+
+  xlab("predicate") +
+  facet_wrap(~class,scale="free_x") +
+  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))
+pred_plot
+ggsave("../results/pred_plot.pdf",height=5)
+
+
