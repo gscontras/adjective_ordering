3a55363bb5f7087f3382f4854e3f502c243d1364
gscontras <scontras@stanford.edu>
2016-04-21 14:56:16 -0700

    noun plots

--- experiments/14-inherentness/results/analysis.R
+++ experiments/14-inherentness/results/analysis.R
@@ -14,7 +14,7 @@ df = do.call(rbind, lapply(1:num_round_dirs, function(i) {
       mutate(workerid = (workerid + (i-1)*9)))}))
 
 #d = subset(df, select=c("workerid", "class","predicate","slide_number","response","condition"))
-d = subset(df, select=c("workerid", "class","predicate","slide_number","response","language","condition"))
+d = subset(df, select=c("workerid", "class","predicate","noun","slide_number","response","language","condition"))
 unique(d$language)
 #d = d[d$language!="Chinese"&d$language!="Spanish, English"&d$language!="Spanish"&d$language!="Hindi"&d$language!="spanish"&d$language!="",]
 #unique(d$language)
@@ -46,33 +46,31 @@ class_plot
 #ggsave("../results/class_ratings.pdf",height=3)
 
 
-
-#### just subjectivity
-
-
 ## predicate plot by class
-c_s = bootsSummary(data=d, measurevar="response", groupvars=c("class"))
-class_plot <- ggplot(c_s, aes(x=reorder(class,-response,mean),y=response)) +
-  geom_bar(stat="identity",position=position_dodge()) +
-  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=reorder(class,response,is.ordered=T), width=0.1),position=position_dodge(width=0.9))+
-  ylab("subjectivity\n")+
-  xlab("adjective class") +
-  #facet_wrap(~class,scale="free_x") +
-  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))
-class_plot
-ggsave("../results/class_plot.pdf",height=5)
-
-## predicate plot by class
-p_s = bootsSummary(data=d, measurevar="response", groupvars=c("class","predicate"))
+p_s = bootsSummary(data=inh, measurevar="response", groupvars=c("class","predicate","noun"))
 p_s$predicate <- factor(p_s$predicate,ordered=is.ordered(p_s$predicate))
-pred_plot <- ggplot(p_s, aes(x=reorder(predicate,-response,mean),y=response)) +
-  geom_bar(stat="identity",position=position_dodge()) +
-  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=reorder(predicate,response,is.ordered=T), width=0.1),position=position_dodge(width=0.9))+
+pred_plot <- ggplot(p_s, aes(x=noun,y=response)) +
+  geom_bar(stat="identity",position=position_dodge(),fill="gray",color="darkgray") +
+  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=noun, width=0.1),position=position_dodge(width=0.9))+
+  ylab("inherentness\n")+
+  xlab("predicate") +
+  facet_wrap(class~predicate,scale="free_x") +
+  theme_bw()+
+  theme(axis.text.x=element_text(angle=90,vjust=0.5,hjust=1))
+pred_plot
+ggsave("../results/inh_pred_plot.pdf",height=8, width=9)
+
+
+s_p_s = bootsSummary(data=sub, measurevar="response", groupvars=c("class","predicate","noun"))
+s_pred_plot <- ggplot(s_p_s, aes(x=noun,y=response)) +
+  geom_bar(stat="identity",position=position_dodge(),fill="gray",color="darkgray") +
+  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=noun, width=0.1),position=position_dodge(width=0.9))+
   ylab("subjectivity\n")+
   xlab("predicate") +
-  facet_wrap(~class,scale="free_x") +
-  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))
-pred_plot
-ggsave("../results/pred_plot.pdf",height=5)
+  facet_wrap(class~predicate,scale="free_x") +
+  theme_bw()+
+  theme(axis.text.x=element_text(angle=90,vjust=0.5,hjust=1))
+s_pred_plot
+ggsave("../results/sub_pred_plot.pdf",height=8, width=9)
 
-
+summary(s_p_s)
