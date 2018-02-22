32d8282fe2da794559aa2d04423314c72b75a815
gscontras <gscontras@gmail.com>
2018-02-22 13:41:05 -0800

    misclassifications math

--- writing/ordering/revision1/probabilities.r
+++ writing/ordering/revision1/probabilities.r
@@ -13,7 +13,21 @@
 # p(no error) = (1-epsilon)^N1 * (1-epsilon)^N2 * ...
 
 # subjectivity ordering
-1-((1-0.01)^59)*((1-0.02)^33)*((1-0.03)^15)
+((1-0.01)^59)*((1-0.02)^33)*((1-0.03)^15)
 
 # reverse subjectivity ordering
-1-((1-0.01)^15)*((1-0.02)^33)*((1-0.03)^59)
+((1-0.01)^15)*((1-0.02)^33)*((1-0.03)^59)
+
+
+# subjectivity ordering
+1-((1-0.1)^8)*((1-0.2)^4)*((1-0.3)^2)
+
+# reverse subjectivity ordering
+1-((1-0.3)^8)*((1-0.2)^4)*((1-0.1)^2)
+
+
+
+## 59 boxes
+## 33 small boxes
+## 15 brown small boxes
+##  3 carboard brown small boxes
