bf4d98eb79c426341c51a7adba95ee0b269f9162
gscontras <gscontras@gmail.com>
2018-06-19 13:30:56 -0700

    added class distance plot

--- experiments/8-order-preference-and/results/analysis.R
+++ experiments/8-order-preference-and/results/analysis.R
@@ -2,10 +2,58 @@ library(ggplot2)
 library(reshape2)
 library(lme4)
 
-setwd("~/Documents/git/cocolab/adjective_ordering/experiments/8-order-preference-and/Submiterator-master")
+#setwd("~/Documents/git/cocolab/adjective_ordering/experiments/8-order-preference-and/Submiterator-master")
+setwd("~/git/adjective_ordering/experiments/8-order-preference-and/Submiterator-master")
 
 d = read.table("order-preference-trials.tsv",sep="\t",header=T)
 head(d)
+
+o <- d
+o$rightpredicate1 = o$predicate2
+o$rightpredicate2 = o$predicate1
+o$rightresponse = 1-o$response
+agr = o %>% 
+  select(predicate1,rightpredicate1,response,rightresponse,workerid,noun,nounclass,class1,class2) %>%
+  gather(predicateposition,predicate,predicate1:rightpredicate1,-workerid,-noun,-nounclass,-class1,-class2)
+agr$correctresponse = agr$response
+agr[agr$predicateposition == "rightpredicate1",]$correctresponse = agr[agr$predicateposition == "rightpredicate1",]$rightresponse
+agr$correctclass = agr$class1
+agr[agr$predicateposition == "rightpredicate1",]$correctclass = agr[agr$predicateposition == "rightpredicate1",]$class2
+head(agr[agr$predicateposition == "rightpredicate1",])
+agr$response = NULL
+agr$rightresponse = NULL
+agr$class1 = NULL
+agr$class2 = NULL
+nrow(agr) #3600
+
+
+adj_agr = aggregate(correctresponse~predicate*correctclass,FUN=mean,data=agr)
+adj_agr
+
+class_agr = aggregate(correctresponse~correctclass,FUN=mean,data=agr)
+
+#source("../results/helpers.r")
+
+class_s = bootsSummary(data=agr, measurevar="correctresponse", groupvars=c("correctclass"))
+
+ggplot(data=class_s,aes(x=reorder(correctclass,-correctresponse,mean),y=correctresponse))+
+  geom_bar(stat="identity")+
+  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=reorder(correctclass,-correctresponse,mean), width=0.1),alpha=0.5)+
+  xlab("\nadjective class")+
+  ylab("distance from noun\n")+
+  ylim(0,1)+
+  #labs("order\npreference")+
+  theme_bw()#+
+#theme(axis.text.x=element_text(angle=90,vjust=0.35,hjust=1))
+#ggsave("../results/class_distance.pdf",height=3)
+
+
+
+
+
+
+
+
 old_d = read.table("~/Documents/git/cocolab/adjective_ordering/experiments/3-order-preference/Submiterator-master/order-preference-trials.tsv",sep="\t",header=T)
 old_d$modification = "original\nexperiment"
 head(old_d)
