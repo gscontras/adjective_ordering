4dc872c37a29002c1ac0d15238c79f727510a783
gscontras <gscontras@gmail.com>
2018-07-13 15:45:56 -0700

    revision2

--- writing/ordering/revision1/probabilities.r
+++ writing/ordering/revision1/probabilities.r
@@ -13,10 +13,10 @@
 # p(no error) = (1-epsilon)^N1 * (1-epsilon)^N2 * ...
 
 # subjectivity ordering
-((1-0.01)^59)*((1-0.02)^33)*((1-0.03)^15)
+1-((1-0.01)^59)*((1-0.02)^33)*((1-0.03)^15)
 
 # reverse subjectivity ordering
-((1-0.01)^15)*((1-0.02)^33)*((1-0.03)^59)
+1-((1-0.01)^15)*((1-0.02)^33)*((1-0.03)^59)
 
 
 # subjectivity ordering
@@ -25,9 +25,8 @@
 # reverse subjectivity ordering
 1-((1-0.3)^8)*((1-0.2)^4)*((1-0.1)^2)
 
+# small brown
+1-((1-0.2)^4 * (1-0.64)^2)
 
-
-## 59 boxes
-## 33 small boxes
-## 15 brown small boxes
-##  3 carboard brown small boxes
+# brown small
+1-((1-0.2)^2 * (1-0.64)^4)
